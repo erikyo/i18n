da8e9af674f0c35302cfe9cba5ee295554a814ca
Erik Golinelli <erik@codekraft.it>
2024-05-08 17:50:04 +0200

    strictly typed sprintf

--- src/sprintf.ts
+++ src/sprintf.ts
@@ -1,23 +1,49 @@
 import tSprintf from "@tannin/sprintf";
 
+type Specifiers = {
+    's': string,
+    'd': number,
+    'b': boolean,
+    'D': Date
+};
+type S = keyof Specifiers;
+
+type ExtractNamedPlaceholders<T extends string> =
+    T extends `${infer Start}%(${infer Key})${infer Spec}${infer Rest}`
+        ? Spec extends S
+            ? { [K in Key]: Specifiers[Spec]} & ExtractNamedPlaceholders<Rest>
+            : never
+        : {};
+
+type ExtractUnnamedPlaceholders<T extends string> =
+    T extends `${infer Start}%${infer Spec}${infer Rest}`
+        ? Spec extends S
+            ? [Specifiers[Spec], ...ExtractUnnamedPlaceholders<Rest>]
+            : never
+        : [];
+
+type SprintfArgs<T extends string> =
+    ExtractUnnamedPlaceholders<T> extends never
+        ? [values: ExtractNamedPlaceholders<T>]
+        : ExtractUnnamedPlaceholders<T>;
+
+
 /**
  * Returns a formatted string. If an error occurs in applying the format, the
  * original format string is returned.
  *
  * @param format The format of the string to generate.
- * @param args   Arguments to apply to the format.
- *
- * @see https://www.npmjs.com/package/sprintf-js
+ * @param values   Arguments to apply to the format.
  *
  * @return {string} The formatted string.
  */
-export function sprintf(
-	format: string,
-	...args: (string | string[] | { [x: string]: string })[]
+export function sprintf<T extends string>(
+    format: T,
+    ...values: SprintfArgs<T>
 ): string {
-	try {
-		return tSprintf(format, ...args);
-	} catch (error) {
-		return format;
-	}
+    try {
+        return tSprintf(format, ...values);
+    } catch (error) {
+        return format;
+    }
 }
--- tests/sprintf.test.ts
+++ tests/sprintf.test.ts
@@ -6,29 +6,45 @@ import { vi } from "vitest";
 import { sprintf } from "../src";
 
 describe("i18n", () => {
-	describe("sprintf", () => {
-		it("absorbs errors", () => {
-			// Mock memoization as identity function. Inline since Jest errors on out-of-scope references in a mock callback.
-			const consoleMock = vi.spyOn(console, "error");
-			// Disable reason: Failing case is the purpose of the test.
-			// eslint-disable-next-line @wordpress/valid-sprintf
-			const result = sprintf("Hello %(placeholder-not-provided)s");
+    describe("sprintf", () => {
+        it("absorbs errors", () => {
+            // Mock memoization as identity function. Inline since Jest errors on out-of-scope references in a mock callback.
+            const consoleMock = vi.spyOn(console, "error");
+            // Disable reason: Failing case is the purpose of the test.
+            // eslint-disable-next-line @wordpress/valid-sprintf
+            const result = sprintf("Hello %(placeholder-not-provided)s");
 
-			expect(consoleMock).not.toThrowError();
-			expect(consoleMock).toHaveBeenCalledOnce();
-			expect(result).toBe("Hello %(placeholder-not-provided)s");
-		});
+            expect(consoleMock).not.toThrowError();
+            expect(consoleMock).toHaveBeenCalledOnce();
+            expect(result).toBe("Hello %(placeholder-not-provided)s");
+        });
 
-		it("replaces placeholders", () => {
-			const result = sprintf("bonjour %s", "Riad");
+        it("replaces placeholders", () => {
+            const result = sprintf("bonjour %s", "erik");
+            expect(result).toBe("bonjour Riad");
+        });
 
-			expect(result).toBe("bonjour Riad");
-		});
+        it("replaces named placeholders", () => {
+            const result = sprintf("bonjour %(names)s", { names: "erik" });
 
-		it("replaces named placeholders", () => {
-			const result = sprintf("bonjour %(name)s", { name: "Riad" });
 
-			expect(result).toBe("bonjour Riad");
-		});
-	});
+            expect(result).toBe("bonjour Riad");
+        });
+    });
 });
+
+
+describe("tests sprintf types", () => {
+    it("it works as expected", () => {
+        sprintf('this is a %s and it is %d years old, right?%b %D', 'erik', 20, true, new Date()); // ok, it checks for different types
+        sprintf('this is a %s and it is %b years old, right?%d %D', 'erik', 20, true, new Date()); // not ok, wrong type
+        sprintf('this is a %s and it is %b%b%b%byears old, right?%d %D', 'erik', 20, true, new Date()); // not ok, too many arguments
+        sprintf('Hello %s', 'John'); // ok
+        sprintf('Hello %s', 'John', 'Doe'); // not ok, too many arguments
+        sprintf('Hello %s', false); // not ok, wrong type
+        sprintf('Hello %s', false); // not ok, wrong type
+        sprintf("Ciao %(names)s", {names: "erik"}) // ok named placeholder
+        sprintf("Ciao %(names)s %(years)d", {names: "erik", years: 20});// ok multiple named placeholder
+        sprintf("Ciao %(names)s %(years)d", {names: "erik", years: "20"});// not ok, wrong type
+    });
+})
