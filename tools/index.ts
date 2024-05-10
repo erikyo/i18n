5fd656626ce74c6b88b39eda9f2c3e84360c3e73
Erik Golinelli <erik@codekraft.it>
2024-05-10 18:57:58 +0200

    adds the makePot cli command

--- tools/index.ts
+++ tools/index.ts
@@ -4,6 +4,14 @@
  */
 import {convertPOTToPHP} from "./convertTranslationToPHP.js";
 
+/**
+ * Fire the conversion from POT to PHP.
+ *
+ * @var {Array} args - The options for the conversion.
+ * @property {string} args[0] potFile - The path or identifier of the POT file.
+ * @property {string} args[1] phpFile - The path or identifier of the PHP file.
+ * @property {string} args[2] textdomain - The text domain.
+ */
 const args: string[] = process.argv.slice(2);
 
 convertPOTToPHP(args[0], args[1], {
